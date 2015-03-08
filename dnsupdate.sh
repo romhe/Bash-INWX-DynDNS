fec80bb3178989fc317cc65eac5589dffe87b57b
romhe <r.hecht@stud.hs-wismar.de>
2015-03-08 21:30:40 +0100

    Update dnsupdate.sh
    curlmyip.com is often down and there was no error-handling at all in this script. Therefore I simply implemented another service and a basic form of "error-handling" :-).

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -7,9 +7,16 @@ APIHOST="https://api.domrobot.com/xmlrpc/"
 OLDIP=$(cat old.ip)
 NEWIP=$(curl -s curlmyip.com)
 
-if [ ! "$OLDIP" == "$NEWIP" ]; then
+#If we did not get an IP because the service is down we try another one
+if [ -z "$NEWIP" ]; then
+  NEWIP=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+fi
+
+if [ ! "$OLDIP" == "$NEWIP" -a -n "$NEWIP" ]; then
     echo $NEWIP > old.ip
     DATA=$(cat update.api | sed "s/%PASSWD%/$PASSWORD/g;s/%USER%/$USERNAME/g;s/%DNSID%/$DNSID/g;s/%NEWIP%/$NEWIP/g")
     curl  -X POST -d "$DATA" "$APIHOST" --header "Content-Type:text/xml" --insecure > update.log
+else
+    echo "Error: Could not get ip" > update.log
 fi
 
